d19ac7a2fff9bd284bcf99b09bff620ea1d1bc46
mansurissa <mansurissa6@gmail.com>
2020-06-16 20:27:51 +0200

    installing morgan to see logs in my terminal

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+
+const morgan = require('morgan');
+
 const productRouter = require('./api/routes/products');
-const ordersRouter = require('./api/routes/orders')
+const ordersRouter = require('./api/routes/orders');
+
+app.use(morgan())
 
 app.use('/products', productRouter);
 app.use('/orders', ordersRouter)
