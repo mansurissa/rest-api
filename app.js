4d015b686e57d50f0d0b99dbe5ff271095cae107
mansurissa <mansurissa6@gmail.com>
2020-06-22 20:29:34 +0200

    resolving error

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ const ordersRouter = require('./api/routes/orders');
 app.use(morgan('dev'))
 
 app.use('/products', productRouter);
-app.use('/orders', ordersRouter)
+app.use('/orders', ordersRouter);
+
+//error handling 
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
