3313db66029eb4c26a9175c4fd86be9e113f7f0e
mansurissa <mansurissa6@gmail.com>
2020-06-27 10:21:37 +0200

    validating order before storing it

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,24 +3,38 @@ const orders = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../models/ordersModel');
+const Product = require('../models/productModel')
 
 orders.post('/', (req, res, next) => {
-    const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.ObjectId
-    });
-    order
-        .save()
-        .then(result => {
-            res.json(result)
+    Product.findById(req.body.productId)
+        .exec()
+        .then(product => {
+            if (!product) {
+                res.json({
+                    message: "no product found"
+                })
+            } else {
+                const order = new Order({
+                    _id: mongoose.Types.ObjectId(),
+                    quantity: req.body.quantity,
+                    product: req.body.ObjectId
+                });
+                order
+                    .save()
+                    .then(result => {
+                        res.json(result)
+                    }
+                    )
+                    .catch(err => {
+                        res.status(500).json({
+                            error: err
+                        })
+                    })
+            }
         }
+
         )
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            })
-        })
+
 
 });
 
