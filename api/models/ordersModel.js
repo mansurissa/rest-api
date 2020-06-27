53daff5c4c91fc28198fe1505ecc5031c86c4260
mansurissa <mansurissa6@gmail.com>
2020-06-27 08:39:19 +0200

    creating a model of orders

---
+++ api/models/ordersModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+    quantity: { type: Number, required: true }
+})
+
+module.exports = mongoose.model('Order', productSchema)
