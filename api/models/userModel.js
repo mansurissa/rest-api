8a42d2ec220bff1639a214475e4c7cee024b39d5
mansurissa <mansurissa6@gmail.com>
2020-07-03 10:04:26 +0200

    creating  user route

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
+const usersSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId(),
-
-
-
     email: { type: String, required: true },
     password: { type: Number, required: true }
 })
+
+module.exports = mongoose.model('Users', usersSchema)
